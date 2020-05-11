f89ae8d7b04aceffd12676912131c286b5f0d4a9
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-11 16:21:35 -0500

    q's pass with array then changed the code to make it work with linked lists and tests pass

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,88 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+class Node:
+  def __init__(self, value=None, next_node=None):
+    # the value at this linked list node
+    self.value = value
+    # reference to the next node in the list
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    # set this node's next_node reference to the passed in node
+    self.next_node = new_next
+
+  def __str__(self):
+      return f'{self.value},{self.next_node}'
+
+class LinkedList:
+    def __init__(self):
+        # first node in the list 
+        self.head = None
+
+    def add_to_end(self, value):
+        # regardless of if the list is empty or not, we need to wrap the value in a Node 
+        new_node = Node(value)
+        # what if the list is empty? 
+        if not self.head:
+            self.head = new_node
+        # what if the list isn't empty?
+        else:
+            # what node do we want to add the new node to? 
+            # the last node in the list 
+            # we can get to the last node in the list by traversing it 
+            current = self.head 
+            while current.get_next() is not None:
+                current = current.get_next()
+            # we're at the end of the linked list 
+            current.set_next(new_node)
+
+    def remove_from_head(self):
+        # what if the list is empty?
+        if not self.head:
+            return None
+        # what if it isn't empty?
+        else:
+            # we want to return the value at the current head 
+            value = self.head.get_value()
+            # remove the value at the head 
+            # update self.head 
+            self.head = self.head.get_next()
+            return value
+
+    def getCount(self):
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.get_next()
+        return count
+
+    def __str__(self):
+        return f'{self.head}'
+
 class Queue:
     def __init__(self):
+        self.storage = LinkedList()
         self.size = 0
-        # self.storage = ?
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_end(value)
+        self.size = self.storage.getCount()
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            value = self.storage.remove_from_head()
+            self.size = self.storage.getCount()
+            return value
