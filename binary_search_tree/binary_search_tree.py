b213966864ad0d534a5c6d434e1a6d0b94c71feb
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-13 17:54:26 -0500

    feels like i'm close to getting these to work

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,27 +17,59 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if self.left is None and self.right is None:
+            self.value = value
+        else:
+            if value < self.value:
+                if self.left is None:
+                    self.left = value
+                else: 
+                    self.insert(value)
+            else:
+                if self.right is None:
+                    self.right = value
+                else:
+                    self.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        #only be moving to the right side
+        #checking until the last node's right is None 
+        #if the last node's right is None that is the largest and retun it's value
+        current = self
+        while current.right:
+            current = current.right
+        return current.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        #traverse each node to the left until it reaches none
+        #traverse each node to the right until it reaches none
         pass
-
+    
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+       if node:
+           self.in_order_print(node.left)
+           print(node.value)
+           self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -59,3 +91,11 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+    def __str__(self):
+        return f'{self.value}'
+
+root = BSTNode(5)
+# root.insert(BSTNode(4))
+
+root.in_order_print(root)
